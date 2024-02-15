f117d35c381649a395a4a0690f71cbc479e3a69e
thomas <thomas@thomass-MacBook-Pro.local>
2024-02-15 19:00:58 +0000

    update

--- src/pl_sandbox/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Root package info."""
-
-import os
-
-from pl_sandbox.__about__ import *  # noqa: F401, F403
-
-_PACKAGE_ROOT = os.path.dirname(__file__)
-_PROJECT_ROOT = os.path.dirname(_PACKAGE_ROOT)
