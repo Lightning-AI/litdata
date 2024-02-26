7509cdfd4d7e87d58d58d7e988a8e0025466d2cc
Mads Oestergaard <mads.oestergaard@wsa.com>
2024-02-26 12:51:56 +0100

    Add a cached local downloader

--- litdata/streaming/resolver.py
+++ litdata/streaming/resolver.py
@@ -55,6 +55,9 @@ def _resolve_dir(dir_path: Optional[Union[str, Dir]]) -> Dir:
 
     if dir_path.startswith("s3://"):
         return Dir(path=None, url=dir_path)
+    
+    if dir_path.startswith("local:"):
+        return Dir(path=None, url=dir_path)
 
     dir_path = _resolve_time_template(dir_path)
 
