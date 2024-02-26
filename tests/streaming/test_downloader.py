6142007bdcbd511be45ed5499871b6597d491685
Mads Oestergaard <mads.oestergaard@wsa.com>
2024-02-26 12:52:10 +0100

    Test of cached local downloader

--- tests/streaming/test_downloader.py
+++ tests/streaming/test_downloader.py
@@ -1,7 +1,7 @@
 import os
 from unittest.mock import MagicMock
 
-from litdata.streaming.downloader import S3Downloader, subprocess
+from litdata.streaming.downloader import S3Downloader, LocalDownloaderWithCache, subprocess, shutil
 
 
 def test_s3_downloader_fast(tmpdir, monkeypatch):
@@ -11,3 +11,17 @@ def test_s3_downloader_fast(tmpdir, monkeypatch):
     downloader = S3Downloader(tmpdir, tmpdir, [])
     downloader.download_file("s3://random_bucket/a.txt", os.path.join(tmpdir, "a.txt"))
     popen_mock.wait.assert_called()
+
+def test_download_with_cache(tmpdir, monkeypatch):
+    # Create a file to download/cache
+    with open("a.txt", "w") as f:
+        f.write("hello")
+
+    try:
+        local_downloader = LocalDownloaderWithCache(tmpdir, tmpdir, [])
+        shutil_mock = MagicMock()
+        monkeypatch.setattr(shutil, "copy", shutil_mock)
+        local_downloader.download_file("local:a.txt", os.path.join(tmpdir, "a.txt"))
+        shutil_mock.assert_called()
+    finally:
+        os.remove("a.txt")
