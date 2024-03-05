1fb6d321767d16af7ba02ffce39c9f05e8038461
thomas chaton <thomas.chaton.ai@gmail.com>
2024-03-05 09:53:13 +0000

    Update URLs for execute (#58)

--- litdata/streaming/resolver.py
+++ litdata/streaming/resolver.py
@@ -333,8 +333,8 @@ def _execute(
         )
         if not has_printed:
             cloud_url = os.getenv("LIGHTNING_CLOUD_URL", "https://lightning.ai").replace(":443", "")
-            job_url = f"{cloud_url}/{studio.owner}/{studio._teamspace.name}"
-            job_url += f"/studios/{studio.name}/app?app_id=data-prep&job_name={curr_job.name}"
+            job_url = f"{cloud_url}/{studio.owner.name}/{studio._teamspace.name}"
+            job_url += f"/studios/{studio.name}/app?app_id=litdata&app_tab=Runs&job_name={curr_job.name}"
             print(f"Find your job at {job_url}")
             has_printed = True
 
