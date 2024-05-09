7fe1c2674d14f190fabf089fe51c9b6e91dacef8
enrico-stauss <73635664+enrico-stauss@users.noreply.github.com>
2024-05-09 11:55:39 +0200

    Fix the NoHeaderTensorSerializer for 1D tensors (other than tokens) (#124)
    * Fix the NoHeaderTensorSerializer for 1D tensors (other than tokens)
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Implement an end to end test for the deserialization of a NoHeaderTensor with the PyTreeLoader
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update
    
    * update
    
    * update
    
    ---------
    
    Co-authored-by: Enrico Stauss <enrico_sta@web.de>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: tchaton <thomas.chaton.ai@gmail.com>

--- tests/streaming/test_item_loader.py
+++ tests/streaming/test_item_loader.py
@@ -1,5 +1,9 @@
 from unittest.mock import MagicMock
 
+import torch
+from litdata.constants import _TORCH_DTYPES_MAPPING
+from litdata.streaming import Cache
+from litdata.streaming.dataset import StreamingDataset
 from litdata.streaming.item_loader import PyTreeLoader
 
 
@@ -9,4 +13,28 @@ def test_serializer_setup():
     serializer_mock = MagicMock()
     item_loader = PyTreeLoader()
     item_loader.setup(config_mock, [], {"fake": serializer_mock})
-    serializer_mock.setup._mock_mock_calls[0].args[0] == "fake:12"
+    assert len(item_loader._serializers) == 2
+    assert item_loader._serializers["fake:12"]
+
+
+def test_pytreeloader_with_no_header_tensor_serializer(tmpdir):
+    cache = Cache(str(tmpdir), chunk_size=10)
+    assert isinstance(cache._reader._item_loader, PyTreeLoader)
+    dtype_index_float = 1
+    dtype_index_long = 18
+    for i in range(10):
+        cache[i] = {
+            "float": i * torch.ones(10).to(_TORCH_DTYPES_MAPPING[dtype_index_float]),
+            "long": i * torch.ones(10).to(_TORCH_DTYPES_MAPPING[dtype_index_long]),
+        }
+
+    data_format = [f"no_header_tensor:{dtype_index_float}", f"no_header_tensor:{dtype_index_long}"]
+    assert cache._writer.get_config()["data_format"] == data_format
+    cache.done()
+    cache.merge()
+
+    dataset = StreamingDataset(input_dir=str(tmpdir))
+    for i in range(len(dataset)):
+        item = dataset[i]
+        assert torch.allclose(i * torch.ones(10).to(_TORCH_DTYPES_MAPPING[dtype_index_float]), item["float"])
+        assert torch.allclose(i * torch.ones(10).to(_TORCH_DTYPES_MAPPING[dtype_index_long]), item["long"])
