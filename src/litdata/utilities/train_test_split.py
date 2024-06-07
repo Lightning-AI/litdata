68ada57bdb3e5b7f8d76dffb9ea6bd38f9f0109b
Deependu Jha <deependujha21@gmail.com>
2024-06-07 19:50:17 +0000

    split ifs in train_test_split and modify {frac_start and frac_end} to using np.cumsum for tracking fraction to use

--- src/litdata/utilities/train_test_split.py
+++ src/litdata/utilities/train_test_split.py
@@ -1,34 +1,35 @@
-from copy import deepcopy
+import numpy as np
 from typing import List
-
+from copy import deepcopy
 from litdata import StreamingDataset
 
-
-def train_test_split(streaming_dataset: StreamingDataset, splits: List[float]) -> List[StreamingDataset]:
-    """Split a StreamingDataset into multiple subsets for purposes such as training, testing, and validation.
-
-    Arguments:
-        streaming_dataset (StreamingDataset): An instance of StreamingDataset that needs to be split.
-        splits (List[float]): List of floats representing the proportion of data to be allocated to each split
-                            (e.g., [0.8, 0.1, 0.1] for train, test, and validation).
-
-    Returns:
-        List[StreamingDataset]: A list of StreamingDataset instances, each corresponding to the proportions specified
-                                in the splits argument.
-
+def train_test_split(streaming_dataset: StreamingDataset, splits: List[float])->List[StreamingDataset]:
     """
-    if any([not isinstance(split, float) for split in splits]) or not (
-        all([_f > 0 and _f <= 1 for _f in splits]) and sum(splits) <= 1
-    ):
-        raise ValueError("Split should be float with each value in [0,1] and max sum can be 1.")
+        Split a StreamingDataset into multiple subsets for purposes such as training, testing, and validation.
+
+        Arguments:
+            streaming_dataset (StreamingDataset): An instance of StreamingDataset that needs to be split.
+            splits (List[float]): List of floats representing the proportion of data to be allocated to each split
+                                (e.g., [0.8, 0.1, 0.1] for train, test, and validation).
+
+        Returns:
+            List[StreamingDataset]: A list of StreamingDataset instances, each corresponding to the proportions specified
+                                    in the splits argument.
+        """
+    if any(not isinstance(split, float) for split in splits):
+        raise ValueError(f"Each split should be a float.")
+
+    if not all(0 < _f <= 1 for _f in splits):
+        raise ValueError(f"Each Split should be a float with each value in [0,1].")
+
+    if sum(splits) > 1:
+        raise ValueError(f"Splits' sum must be less than 1.")
 
     my_datasets = [deepcopy(streaming_dataset) for _ in splits]
 
-    frac_start = 0
-    frac_end = 0
+
+    fraction = [0] + np.cumsum(splits).tolist()
     for i in range(len(splits)):
-        frac_end += splits[i]
-        my_datasets[i]._modify_subsample_interval(frac_start, frac_end)
-        frac_start += splits[i]
+        my_datasets[i]._modify_subsample_interval(fraction[i],fraction[i+1])
 
     return my_datasets
