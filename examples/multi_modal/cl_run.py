18dc787576dd7022ec0c64a3e750940cdc521cc6
rakro101 <kronberg.raphael@gmail.com>
2024-06-11 15:49:48 +0200

    pr fixes

---
+++ examples/multi_modal/cl_run.py
@@ -0,0 +1,23 @@
+import os
+from generate import generate_data_set
+from create_labelencoder import create_labelencoder
+from convert import pl_optimize
+from train import lightning_training
+from config import HYPERPARAMETERS
+
+def main():
+    output_dir = "dataframe_data"
+    os.makedirs(output_dir, exist_ok=True)
+    generate_data_set(output_dir, num_pds=2, num_entries=10)
+    create_labelencoder()
+    input_dir = "dataframe_data"
+    output_dir = "lightning_data/version_0"
+    pl_optimize(input_dir, output_dir)
+    HYPERPARAMETERS.update({"num_workers": 2})
+    HYPERPARAMETERS.update({"max_epochs": 1})
+    HYPERPARAMETERS.update({"limit_batches": 1})
+    HYPERPARAMETERS.update({"batch_size": 1})
+    lightning_training(model_dir="logs", hyperparameters=HYPERPARAMETERS)
+
+if __name__ == "__main__":
+    main()
