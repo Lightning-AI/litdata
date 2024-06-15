7438b97f23f669cbc0ad9166a02e3b7b4e5f799d
Deependu Jha <deependujha21@gmail.com>
2024-06-15 12:54:44 +0530

    Update src/litdata/streaming/dataset.py
    Co-authored-by: thomas chaton <thomas@grid.ai>

--- src/litdata/streaming/dataset.py
+++ src/litdata/streaming/dataset.py
@@ -71,7 +71,7 @@ class StreamingDataset(IterableDataset):
             raise ValueError(f"Shuffle should be a boolean. Found {shuffle}")
 
         if not isinstance(subsample, float) or subsample < 0 or subsample > 1:
-            raise ValueError("subsample must be a float with value between 0 & 1.")
+            raise ValueError("subsample must be a float with value between 0 and 1.")
 
         input_dir = _resolve_dir(input_dir)
 
