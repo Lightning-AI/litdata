a807bbd38989a02a3b3ac48acaa8d9b31033f119
bhimrazy <bhimrajyadav977@gmail.com>
2024-07-11 02:35:21 +0545

    feat: Add tests for Fernet and RSA encryption

---
+++ tests/utilities/test_encryption.py
@@ -0,0 +1,33 @@
+from litdata.utilities.encryption import FernetEncryption, RSAEncryption
+
+
+def test_fernet_encryption():
+    password = "password"
+    data = b"test data"
+    fernet = FernetEncryption(password)
+    encrypted_data = fernet.encrypt(data)
+    decrypted_data = fernet.decrypt(encrypted_data)
+    assert data == decrypted_data
+    assert data != encrypted_data
+    assert decrypted_data != encrypted_data
+    assert isinstance(encrypted_data, bytes)
+    assert isinstance(decrypted_data, bytes)
+    assert isinstance(fernet.extension, str)
+    assert fernet.extension == "fernet"
+    assert fernet.passsword == password
+    assert isinstance(fernet._derive_key(password), bytes)
+    assert isinstance(fernet._derive_key(password), bytes)
+
+
+def test_rsa_encryption():
+    data = b"test data"
+    rsa = RSAEncryption()
+    encrypted_data = rsa.encrypt(data)
+    decrypted_data = rsa.decrypt(encrypted_data)
+    assert data == decrypted_data
+    assert data != encrypted_data
+    assert decrypted_data != encrypted_data
+    assert isinstance(encrypted_data, bytes)
+    assert isinstance(decrypted_data, bytes)
+    assert isinstance(rsa.extension, str)
+    assert rsa.extension == "rsa"
