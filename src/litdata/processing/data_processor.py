fd0d9f6392a26aa6924ebdfe96fd6d3fb20500bd
deependujha <deependujha21@gmail.com>
2024-08-06 15:38:44 +0530

    done

--- src/litdata/processing/data_processor.py
+++ src/litdata/processing/data_processor.py
@@ -937,6 +937,8 @@ class DataProcessor:
         """
         # spawn doesn't work in IPython
         start_method = start_method or ("fork" if in_notebook() else "spawn")
+        if not in_notebook() and sys.platform.startswith("linux"):
+            start_method = 'fork'
 
         msg = f"Setting multiprocessing start_method to {start_method}. "
         if in_notebook() and start_method == "fork":
