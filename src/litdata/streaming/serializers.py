8009f3f73f73508fcd3ece6c74f0714bd7ca7a33
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2024-09-18 14:10:17 +0200

    Apply suggestions from code review

--- src/litdata/streaming/serializers.py
+++ src/litdata/streaming/serializers.py
@@ -19,7 +19,7 @@ from abc import ABC, abstractmethod
 from collections import OrderedDict
 from contextlib import suppress
 from copy import deepcopy
-from typing import Any, Dict, Optional, Tuple, Union
+from typing import TYPE_CHECKING, Any, Dict, Optional, Tuple, Union
 
 import numpy as np
 import torch
@@ -27,6 +27,8 @@ from lightning_utilities.core.imports import RequirementCache
 
 from litdata.constants import _NUMPY_DTYPES_MAPPING, _TORCH_DTYPES_MAPPING
 
+if TYPE_CHECKING:
+    from PIL.JpegImagePlugin import JpegImageFile
 _PIL_AVAILABLE = RequirementCache("PIL")
 _TORCH_VISION_AVAILABLE = RequirementCache("torchvision")
 _AV_AVAILABLE = RequirementCache("av")
