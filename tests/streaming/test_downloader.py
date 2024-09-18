e68076d8d05f8b20cc3828598751ccf6b24d7885
deependu <deependujha21@gmail.com>
2024-09-18 10:03:13 +0530

    cleanup commented code

--- tests/streaming/test_downloader.py
+++ tests/streaming/test_downloader.py
@@ -6,14 +6,6 @@ from litdata.streaming.downloader import (
     shutil,
 )
 
-# def test_s3_downloader_fast(tmpdir, monkeypatch):
-#     monkeypatch.setattr(os, "system", MagicMock(return_value=0))
-#     popen_mock = MagicMock()
-#     monkeypatch.setattr(subprocess, "Popen", MagicMock(return_value=popen_mock))
-#     downloader = S3Downloader(tmpdir, tmpdir, [])
-#     downloader.download_file("s3://random_bucket/a.txt", os.path.join(tmpdir, "a.txt"))
-#     popen_mock.wait.assert_called()
-
 
 def test_download_with_cache(tmpdir, monkeypatch):
     # Create a file to download/cache
