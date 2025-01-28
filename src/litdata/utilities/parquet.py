75b3163d71fad68b3456ef1ca999e579c69c7db9
Deependu Jha <deependujha21@gmail.com>
2025-01-28 18:18:14 +0530

    hip-hip hurray. working on google-storage

--- src/litdata/utilities/parquet.py
+++ src/litdata/utilities/parquet.py
@@ -1,4 +1,4 @@
-"""contains utility functions to return parquet files from local, s3, or hf."""
+"""contains utility functions to return parquet files from local, s3, or gs."""
 
 import json
 import os
@@ -8,6 +8,7 @@ from typing import Any, Dict, Generator, List, Optional, Tuple, Type, Union
 
 from litdata.constants import _INDEX_FILENAME
 from litdata.streaming.resolver import Dir, _resolve_dir
+from litdata.utilities.dataset_utilities import _try_create_cache_dir
 
 
 class ParquetDir(ABC):
@@ -59,24 +60,33 @@ class LocalParquetDir(ParquetDir):
         print(f"Index file written to: {os.path.join(self.cache_path, _INDEX_FILENAME)}")
 
 
-class S3ParquetDir(ParquetDir):
+class CloudParquetDir(ParquetDir):
     def __init__(
         self,
         dir_path: Optional[Union[str, Dir]],
         cache_path: Optional[str] = None,
-        storage_options: Optional[Dict] = {},
+        storage_options: Optional[Dict] = None,
     ):
         super().__init__(dir_path, cache_path, storage_options)
+        if self.cache_path is None:
+            self.cache_path = _try_create_cache_dir(self.dir.path)
 
         import fsspec
 
-        # Initialize the S3 filesystem
-        self.fs = fsspec.filesystem("s3", self.storage_options)
+        assert self.dir.url is not None
+
+        _CLOUD_PROVIDER = ("s3", "gs")
+
+        for provider in _CLOUD_PROVIDER:
+            if self.dir.url.startswith(provider):
+                # Initialize the cloud filesystem
+                self.fs = fsspec.filesystem(provider, storage_options=self.storage_options)
+                print(f"using provider: {provider}")
+                break
 
     def __iter__(self) -> Generator[Tuple[str, str], None, None]:
         assert self.dir.url is not None
-        assert self.dir.url.startswith("s3")
-        assert self.cache_dir is not None
+        assert self.cache_path is not None
 
         # List all files and directories in the top-level of the specified directory
         files = self.fs.ls(self.dir.url, detail=True)
@@ -84,47 +94,46 @@ class S3ParquetDir(ParquetDir):
         # Iterate through the items and check for Parquet files
         for file_info in files:
             if file_info["type"] == "file" and file_info["name"].endswith(".parquet"):
-                print(file_info["name"])
                 file_name = os.path.basename(file_info["name"])
                 assert self.cache_path is not None
                 local_path = os.path.join(self.cache_path, file_name)
 
                 # Download the file
-                with self.fs.open(file_info["name"], "rb") as s3_file, open(local_path, "wb") as local_file:
-                    local_file.write(s3_file.read())
+                with self.fs.open(file_info["name"], "rb") as cloud_file, open(local_path, "wb") as local_file:
+                    local_file.write(cloud_file.read())
 
-                print(f"Downloaded {file_info['name']} to {local_path}")
                 yield file_name, local_path
 
     def write_index(self, chunks_info: List[Dict[str, Any]], config: Dict[str, Any]) -> None:
-        assert self.cache_dir is not None
+        assert self.cache_path is not None
         assert self.dir.url is not None
 
-        index_file_path = os.path.join(self.cache_dir, _INDEX_FILENAME)
-        s3_index_path = os.path.join(self.dir.url, _INDEX_FILENAME)
+        index_file_path = os.path.join(self.cache_path, _INDEX_FILENAME)
+        cloud_index_path = os.path.join(self.dir.url, _INDEX_FILENAME)
         # write to index.json file
         with open(index_file_path, "w") as f:
             data = {"chunks": chunks_info, "config": config, "updated_at": str(time())}
             json.dump(data, f, sort_keys=True)
 
-        # upload file to s3
-        with open(index_file_path, "rb") as local_file, self.fs.open(s3_index_path, "wb") as s3_file:
-            s3_file.write(local_file.read())
+        # upload file to cloud
+        with open(index_file_path, "rb") as local_file, self.fs.open(cloud_index_path, "wb") as cloud_file:
+            cloud_file.write(local_file.read())
 
-        print(f"Index file written to: {s3_index_path}")
+        print(f"Index file written to: {cloud_index_path}")
 
 
 _PARQUET_DIR: Dict[str, Type[ParquetDir]] = {
-    "s3://": S3ParquetDir,
+    "s3://": CloudParquetDir,
+    "gs://": CloudParquetDir,
     "local:": LocalParquetDir,
     "": LocalParquetDir,
 }
 
 
 def get_parquet_indexer_cls(
-    dir_path: str, cache_dir: Optional[str] = None, storage_options: Optional[Dict] = {}
+    dir_path: str, cache_path: Optional[str] = None, storage_options: Optional[Dict] = {}
 ) -> ParquetDir:
     for k, cls in _PARQUET_DIR.items():
         if str(dir_path).startswith(k):
-            return cls(dir_path, cache_dir, storage_options)
+            return cls(dir_path, cache_path, storage_options)
     raise ValueError(f"The provided `dir_path` {dir_path} doesn't have a ParquetDir class associated.")
