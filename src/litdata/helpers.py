35e37047a1f459eb9f8f49fa86071f7d194b9dfc
Deependu Jha <deependujha21@gmail.com>
2025-02-04 20:27:41 +0000

    tests added for get_hf_pq_file_download_cmd()

--- src/litdata/helpers.py
+++ src/litdata/helpers.py
@@ -62,8 +62,10 @@ def _check_version_and_prompt_upgrade(curr_version: str) -> None:
     return
 
 
-def get_hf_pq_file_download_cmd(file_url: str, local_path: str, hf_token: Optional[str] = None):
+def get_hf_pq_file_download_cmd(file_url: str, local_path: str, hf_token: Optional[str] = None) -> str:
     """Generate a wget command to download a file from Hugging Face with optional authentication."""
-    hf_token = hf_token or os.getenv("HF_TOKEN")  # Use env variable if not provided
-    headers = f"Authorization: Bearer {hf_token}" if hf_token else ""
-    return f'wget -q --header="{headers}" "{file_url}" -O "{local_path}"'
+    hf_token = hf_token or os.getenv("HF_TOKEN", None)  # Use env variable if not provided
+    if hf_token:
+        headers = f"Authorization: Bearer {hf_token}"
+        return f'wget -q --header="{headers}" "{file_url}" -O "{local_path}"'
+    return f'wget -q "{file_url}" -O "{local_path}"'
