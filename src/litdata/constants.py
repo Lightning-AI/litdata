c6edaa77c6c044582812e1faff560a9cc3d2db01
Deependu <deependujha21@gmail.com>
2025-02-06 14:38:58 +0530

    changes for feat:add-parquet-support PR review (#460)

--- src/litdata/constants.py
+++ src/litdata/constants.py
@@ -36,7 +36,7 @@ _GOOGLE_STORAGE_AVAILABLE = RequirementCache("google.cloud.storage")
 _AZURE_STORAGE_AVAILABLE = RequirementCache("azure.storage.blob")
 _TQDM_AVAILABLE = RequirementCache("tqdm")
 _LIGHTNING_SDK_AVAILABLE = RequirementCache("lightning_sdk")
-_POLARS_AVAILABLE = RequirementCache("polars")
+_POLARS_AVAILABLE = RequirementCache("polars>1.0.0")
 _DEBUG = bool(int(os.getenv("DEBUG", "1")))
 
 _MAX_WAIT_TIME = int(os.getenv("MAX_WAIT_TIME", "120"))
