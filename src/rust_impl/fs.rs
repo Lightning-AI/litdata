ef238b45d030ab921fe7ca8d1ad3165ffb09723e
Deependu Jha <deependujha21@gmail.com>
2025-03-04 11:04:16 +0530

    started working on struct

--- src/rust_impl/fs.rs
+++ src/rust_impl/fs.rs
@@ -34,10 +34,19 @@ pub struct StorageDownloader {
     index_to_bytes: HashMap<usize, Vec<u8>>,
     chunk_index: Vec<usize>,
     sample_index: Vec<Vec<usize>>,
+    chunk_index_offset: HashMap<usize, Vec<u32>>, // since offsets were stored as np.uint32 in the chunks file
+    chunk_index_begin: HashMap<usize, u32>, // for the current order of streaming data, at which index the chunk starts
 }
 
 impl StorageDownloader {
     pub fn new() -> Self {
         // Self { index_to_bytes: HashMap::new() }
+        return StorageDownloader{
+            index_to_bytes: HashMap::new(),
+            chunk_index: Vec::new(),
+            sample_index: Vec::new(),
+            chunk_index_offset: HashMap::new(),
+            chunk_index_begin: HashMap::new(),
+        };
     }
 }
